a1f78590e645e1d38df879ccba5aa20a70b62599
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2025-03-07 17:00:50 +0100

    chore: formating

--- kotaemon-custom/kotaemon/fast_ingestion_pipeline.py
+++ kotaemon-custom/kotaemon/fast_ingestion_pipeline.py
@@ -7,32 +7,32 @@ from kotaemon.embeddings import OpenAIEmbeddings
 from kotaemon.storages import LanceDBDocumentStore, QdrantVectorStore
 #
 
-class IndexingPipeline(VectorIndexing):
 
-    COLLECTION_NAME = 'WSL_collection_test'
+class IndexingPipeline(VectorIndexing):
+    COLLECTION_NAME = "WSL_collection_test"
 
     vector_store: QdrantVectorStore = Param(
-        lazy(QdrantVectorStore).withx(url="http://localhost:6333",
-                                      collection_name=COLLECTION_NAME,
-                                      api_key="None",
-                                      ),
+        lazy(QdrantVectorStore).withx(
+            url="http://localhost:6333",
+            collection_name=COLLECTION_NAME,
+            api_key="None",
+        ),
         ignore_ui=True,
     )
     doc_store: LanceDBDocumentStore = Param(
         lazy(LanceDBDocumentStore).withx(
-            path= './ktem_app_data/user_data/docstore',
-            collection_name= COLLECTION_NAME),
+            path="./ktem_app_data/user_data/docstore", collection_name=COLLECTION_NAME
+        ),
         ignore_ui=True,
     )
     embedding: OpenAIEmbeddings = Param(
         lazy(OpenAIEmbeddings).withx(
-        base_url = "http://172.17.0.1:11434/v1/",
-        model="snowflake-arctic-embed2",
-        api_key= "ollama"
-    ),
-    ignore_ui=True,
+            base_url="http://172.17.0.1:11434/v1/",
+            model="snowflake-arctic-embed2",
+            api_key="ollama",
+        ),
+        ignore_ui=True,
     )
-
 
     def run(self, text: str | list[str], metadatas: dict | list[dict] | None) -> Document:
         """Normally, this indexing pipeline returns nothing. For demonstration,
@@ -41,12 +41,12 @@ class IndexingPipeline(VectorIndexing):
         """
         # --- LOGIC INGESTION PIPELINE ----
 
-        #TODO
+        # TODO
 
         # core extraction
         # inference metadatas
         # summarization
-        # 
+        #
 
         # --- END - LOGIC INGESTION PIPELINE ----
 
@@ -54,10 +54,14 @@ class IndexingPipeline(VectorIndexing):
         super().run(text, metadatas)
 
         return Document(self.vector_store._collection.count())
-    
+
 
 pipeline = IndexingPipeline()
 
-pipeline.run(text=["feedback to further \nimprove his skills and knowledge", "yes ok yes it's a test ok"], 
-             metadatas=[{"key_1":"test"}, {"key_1":"test"}])
-
+pipeline.run(
+    text=[
+        "feedback to further \nimprove his skills and knowledge",
+        "yes ok yes it's a test ok",
+    ],
+    metadatas=[{"key_1": "test"}, {"key_1": "test"}],
+)
