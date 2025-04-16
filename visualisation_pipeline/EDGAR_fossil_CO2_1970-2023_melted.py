5ea7f68895a949a8654ff65d005ac117439ae325
cmc <chenbming@gmail.com>
2025-04-16 11:10:41 +0200

    EDGAR melted table script

--- visualisation_pipeline/EDGAR_fossil_CO2_1970-2023_melted.py
+++ visualisation_pipeline/EDGAR_fossil_CO2_1970-2023_melted.py
@@ -8,9 +8,6 @@ SQLModel.metadata.create_all(engine)
 
 file = pd.read_csv("visualisation_pipeline/data/EDGAR_fossil_CO2_1970-2023.csv", delimiter=';', decimal=",")
 file.drop(columns=["Substance", "EDGAR Country Code"], inplace=True)
-file_t = file.set_index('Country')
-file_t = file_t.transpose().reset_index().rename(columns={'index': 'year'})
-file_t.columns.name = None
-file_t.index.name = None
+file_t = pd.melt(file, id_vars=["Country"], var_name="Year", value_name="CO2 Emissions")
 file_t.dropna(how='all', axis=1, inplace=True)
-file_t.to_sql(name="EDGAR_fossil_CO2_1970-2023", con=engine, if_exists="replace", index=False)
+file_t.to_sql(name="EDGAR_fossil_CO2_1970-2023_melted", con=engine, if_exists="replace", index=False)
