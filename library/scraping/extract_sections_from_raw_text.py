9cc25bef8556c463298a6a79be15bfdf695e3fe4
François BONIFACE <fraboniface@gmail.com>
2026-01-02 11:15:22 +0100

    Write results as jsonlines to avoid memory issues

--- library/scraping/extract_sections_from_raw_text.py
+++ library/scraping/extract_sections_from_raw_text.py
@@ -4,16 +4,14 @@ Takes parquet file with raw_text as input, cleans it and extract sections.
 
 import pandas as pd
 import argparse
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import multiprocessing
 from tqdm import tqdm
+import json
 
 from library.scraping.extract_sections import extract_sections, patterns
 from library.scraping.clean.cleaning_pipeline import cleaning_pipeline
 from library.connectors.s3 import upload_to_s3
-
-
-BASE = "https://sufficiency-library.s3.fr-par.scw.cloud"
 
 
 def process_text(text: str) -> dict[str, str]:
@@ -24,31 +22,25 @@ def process_text(text: str) -> dict[str, str]:
     return sections
 
 
-def process_file(input_file: str, num_workers: int = None):
+def process_file(input_file: str, output_file: str, num_workers: int = None):
     df = pd.read_parquet(input_file)
 
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        sections_list = list(
-            tqdm(
-                executor.map(process_text, df["raw_text"]),
-                total=len(df),
-                desc="Processing texts",
-            )
-        )
+        futures = {
+            executor.submit(process_text, text): idx for idx, text in enumerate(df["text"])
+        }
 
-    sections_df = pd.DataFrame(sections_list)
-    result_df = pd.concat([df.drop(columns=["raw_text"]), sections_df], axis=1)
-    return result_df
+        with open(output_file, "w") as f:
+            for future in tqdm(as_completed(futures), total=len(df), desc="Processing texts"):
+                result = future.result()
+                f.write(json.dumps(result) + "\n")
+                del futures[future]
 
 
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn")
 
     parser = argparse.ArgumentParser(description="Process raw text from parquet file")
-    parser.add_argument(
-        "--folder",
-        help="Input folder on S3 (e.g., documents/batch_1). Should contain file raw_texts.parquet)",
-    )
     parser.add_argument(
         "--workers",
         type=int,
@@ -58,11 +50,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    input_file = BASE + "/" + args.folder + "/raw_texts.parquet"
-    print(f"Processing file: {input_file}")
+    for i in range(1, 8):
+        folder = f"outputs/batch_{i}"
+        input_file = folder + "/raw_texts.parquet"
+        output_file = folder + "/processed_texts.jsonl"
+        print(f"Processing file: {input_file}")
+        process_file(input_file, output_file, num_workers=args.workers)
 
-    result_df = process_file(input_file, num_workers=args.workers)
-    result_df.to_parquet("/tmp/processed_texts.parquet")
-    upload_to_s3("/tmp/processed_texts.parquet", args.folder + "/processed_texts.parquet")
-
-    print(f"Processed data saved as {args.folder}/processed_texts.parquet on S3")
+    print(f"Processed data saved as {output_file}")
