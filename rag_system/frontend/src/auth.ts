c73c1fe6abfed01483fb79f32a1d5756627f12bf
François Boniface <fraboniface@gmail.com>
2026-02-20 16:44:47 +0100

    other fix attempt

--- rag_system/frontend/src/auth.ts
+++ rag_system/frontend/src/auth.ts
@@ -1,7 +1,16 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Keycloak from '@auth/sveltekit/providers/keycloak';
+import { TOPSITE_URL } from '$env/static/private';
 
 export const { handle: authenticationHandle, signIn, signOut } = SvelteKitAuth({
 	providers: [Keycloak],
-	trustHost: true
+	trustHost: true,
+	callbacks: {
+		redirect({ url, baseUrl }) {
+			if (url.startsWith(TOPSITE_URL)) {
+				return url;
+			}
+			return url.startsWith('/') ? `${baseUrl}${url}` : baseUrl;
+		}
+	}
 });
